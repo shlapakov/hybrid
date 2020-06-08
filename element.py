c83264202b45da31f342fd6d7ff9aa6aa9b7a627
fakeshluck <fakeshluck@gmail.com>
2020-06-08 13:15:40 +0300

    add docstrings to base element

--- element.py
+++ element.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 class Element:
-    def __init__(self, number: int = 0, name: str = '', x_pos: float = 0.0):
+    def __init__(self, number: int = 0, name: str = ''):
+        """
+        Base element class for inheritance
+        :param number: positional number of element
+        :param name: type of element
+        """
         self.number = number
         self.name = name
         st.sidebar.subheader(f'{self.name} номер {self.number}')
@@ -11,7 +16,14 @@ class Element:
                                             key=f'error{self.number}')
 
     @staticmethod
-    def spawn_param_field(param_name, key, value: int = 10):
+    def spawn_param_field(param_name: str, key: str, value: int = 10) -> int:
+        """
+        Spawns field to get parameter
+        :param param_name: name of parameter. For example 'Мощность'
+        :param key: unique key for number_input field
+        :param value: default value for field
+        :return: value of selected parameter
+        """
         return st.sidebar.number_input(f'{param_name}', 1, value=value, key=key)
 
 
